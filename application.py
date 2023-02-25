5bde61a69b04fea78a027bbfe89ab44c8a09f4d2
Gautam Manuel <gautam.manuel@gmail.com>
2023-02-25 17:10:44 +0530

    i dont know

--- application.py
+++ application.py
@@ -29,6 +29,6 @@ def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
+    app.run(host="0.0.0.0", port=os.getenv("PORT", default=5000))
     # serve(app, host="0.0.0.0", port=5000)
     # app.run()
