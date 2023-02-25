b9f352412000a366fba32796a1aac62467f2a9aa
Gautam Manuel <gautam.manuel@gmail.com>
2023-02-25 16:57:39 +0530

    update port

--- application.py
+++ application.py
@@ -24,8 +24,11 @@ def ping():
 
     return "erhhehe"
 
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
 if __name__ == '__main__':
-    # port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
     # serve(app, host="0.0.0.0", port=5000)
     # app.run()
