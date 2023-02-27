7f5dcbc0ce43a1b9556fb708cf61c5890af3d54e
Gautam Manuel <gautam.manuel@gmail.com>
2023-02-27 11:19:35 +0530

    strewamlit

--- src/application.py
+++ src/application.py
@@ -1,35 +1,51 @@
 from flask import Flask, request, jsonify
 import os
 from model.predict import *
+import streamlit as st
 
 app = Flask(__name__)
 
-@app.route('/api/create-texture', methods=['POST'])
-def create_texture():   
-    front_image = request.files["front"].read()
-    back_image = request.files["back"].read()
-    primary_color = request.form["color"]
+def main():
+    st.title('Image Segmentation App')
 
-    front_output1 = predict(front_image)
-    # back_output1 = predict(back_image)
-    front_output2 = predict_h5(front_image)
-    back_output2 = predict_h5(back_image)
+    @st.experimental_api(allowed_methods=["POST"])
+    def segment_image(file):
+        front_image = request.files["front"].read()
+        back_image = request.files["back"].read()
+        primary_color = request.form["color"]
 
-    return jsonify({"front": front_output1, "back" : back_output1, "front2": front_output2, "back2" : back_output2})
+        front_output2 = predict_h5(front_image)
+        back_output2 = predict_h5(back_image)
 
-@app.route('/api/ping', methods=['GET'])
-def ping():
-    dir = os.getcwd()   
-    print(dir)
-    dir += "//model"
-    print(os.listdir(dir))
+        return ({"front2": front_output2, "back2" : back_output2})
 
-    return "erhhehe"
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# @app.route('/api/create-texture', methods=['POST'])
+# def create_texture():   
+#     front_image = request.files["front"].read()
+#     back_image = request.files["back"].read()
+#     primary_color = request.form["color"]
+
+#     front_output1 = predict(front_image)
+#     # back_output1 = predict(back_image)
+#     front_output2 = predict_h5(front_image)
+#     back_output2 = predict_h5(back_image)
+
+#     return jsonify({"front": front_output1, "back" : back_output1, "front2": front_output2, "back2" : back_output2})
+
+# @app.route('/api/ping', methods=['GET'])
+# def ping():
+#     dir = os.getcwd()   
+#     print(dir)
+#     dir += "//model"
+#     print(os.listdir(dir))
+
+#     return "erhhehe"
+
+# @app.route('/')
+# def index():
+#     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
+    main()
     # app.run(debug=True)
